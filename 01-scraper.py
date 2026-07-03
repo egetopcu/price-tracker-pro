b15f44c5d661a0f8253c1ea9f9e20654ce350377
egetopcu <115668374+egetopcu@users.noreply.github.com>
2026-07-03 12:41:58 +0300

    trying to find price on 3 dif types

---
+++ 01-scraper.py
@@ -0,0 +1,23 @@
+from hmac import new
+import requests, json
+from bs4 import BeautifulSoup
+
+headers = {"User-Agent": "Mozilla/5.0"}
+
+url="https://www.beymen.com/tr/p_golden-goose-super-star-siyah-erkek-deri-sneaker_1881351"
+
+resp = requests.get(url, headers=headers)
+soup = BeautifulSoup(resp.text, "html.parser")
+
+
+new_tag = soup.find(id="priceNew")
+raw_text = new_tag.text.strip()
+
+old_tag = soup.find(id="priceOld")
+raw_text1 = old_tag.text.strip()
+
+campaign_div = soup.find("div", class_="m-price__campaign")
+campaign_price_tag = campaign_div.find("span", class_="m-price__campaignPrice")
+raw_text2 = campaign_price_tag.text.strip()
+
+print(raw_text,raw_text1,raw_text2)
