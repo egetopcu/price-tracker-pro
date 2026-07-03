80361df1bc326d30c5ae46fdf99ae2d22ecac39b
egetopcu <115668374+egetopcu@users.noreply.github.com>
2026-07-03 12:55:10 +0300

    able to scrape all 4 different types of pages

--- 01-scraper.py
+++ 01-scraper.py
@@ -14,10 +14,16 @@ new_tag = soup.find(id="priceNew")
 raw_text = new_tag.text.strip()
 
 old_tag = soup.find(id="priceOld")
-raw_text1 = old_tag.text.strip()
+if old_tag:
+    raw_text1 = old_tag.text.strip()
+else:
+    raw_text1=None
 
 campaign_div = soup.find("div", class_="m-price__campaign")
-campaign_price_tag = campaign_div.find("span", class_="m-price__campaignPrice")
-raw_text2 = campaign_price_tag.text.strip()
+if campaign_div:
+    campaign_price_tag = campaign_div.find("span", class_="m-price__campaignPrice")
+    raw_text2 = campaign_price_tag.text.strip()
+else:
+    raw_text2=None
 
 print(raw_text,raw_text1,raw_text2)
